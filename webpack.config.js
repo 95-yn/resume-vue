6615de19e97166998cb1d4702bd317e5683fb7ce
yyyyn <95.yyyyn@gmail.com>
2020-09-02 16:29:38 +0800

    add sentry

--- webpack.config.js
+++ webpack.config.js
@@ -94,7 +94,7 @@ module.exports = {
         ]
     },
     mode: 'production',
-    devtool: "cheap-module-source-map",
+    devtool: "source-map",
     devServer: {
         // 配置host及端口
         host: '127.0.0.1',
